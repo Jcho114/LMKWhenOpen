c862111a157709bc7511e2a0b7377542d12f7045
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2023-04-28 14:12:05 -0400

    Functional bot with limited features

--- bot.js
+++ bot.js
@@ -1,11 +1,31 @@
 import puppeteer from "puppeteer";
 import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
 
-(async () => {
-    const browser = await puppeteer.launch({headless: true});
+const scrapeSeats = async () => {
+    const browser = await puppeteer.launch({ headless: 'new'});
     const page = await browser.newPage();
 
-    await page.goto();
+    await page.goto(process.env.LINK);
+    await page.setViewport({width: 1080, height: 1024});
 
+    await page.waitForSelector('.open-seats-count', {
+        visible: true
+    });
 
-})
+    // await page.screenshot({ path: '1.png'});
+
+    const data = await page.evaluate(() => {
+        const element = document.querySelector('.open-seats-count');
+        const seats = element.innerHTML;
+        return seats;
+    });
+
+    await browser.close();
+    console.log("Open seats for JWG CMSC351: " + data);
+    return data;
+}
+
+const minutes = 1;
+setInterval( scrapeSeats, 60000 * minutes );
