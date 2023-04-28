f57579f9f810ab58bf2ad6c5820c9615330e559a
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2023-04-28 18:58:38 -0400

    created basic scheduler

--- bot.js
+++ bot.js
@@ -2,38 +2,47 @@ import puppeteer from "puppeteer";
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import sendMessage from "./send-sms";
+import sendMessage from "./send-sms.js";
 
 const scrapeSeats = async () => {
-    const browser = await puppeteer.launch({ headless: 'new'});
-    const page = await browser.newPage();
+    console.log(new Date().toString() + ": started scraping");
+    const data = 0;
 
-    await page.goto(process.env.LINK);
-    await page.setViewport({width: 1080, height: 1024});
+    try {
+        const browser = await puppeteer.launch({ headless: 'new'});
+        const page = await browser.newPage();
 
-    await page.waitForSelector('.open-seats-count', {
-        visible: true
-    });
+        await page.goto(process.env.LINK);
+        await page.setViewport({width: 1080, height: 1024});
 
-    const data = await page.evaluate(() => {
-        const element = document.querySelector('.open-seats-count');
-        const seats = element.innerHTML;
-        return seats;
-    });
+        await page.waitForSelector('.open-seats-count', {
+            visible: true
+        });
 
-    await browser.close();
-    console.log("Open seats for JWG CMSC351: " + data);
-    if (data > 0) {
-        sendMessage('Seats open for JWG CMSC351');
-        await page.screenshot({ path: '1.png'});
+        const data = await page.evaluate(() => {
+            const element = document.querySelector('.open-seats-count');
+            const seats = element.innerHTML;
+            return seats;
+        });
+
+        await browser.close();
+        console.log(new Date().toString() + "open seats for JWG CMSC351: " + data);
+        if (data > 0) {
+            sendMessage('Seats open for JWG CMSC351');
+            console.log(new Date().toString() + "sent message to user");
+            await page.screenshot({ path: '1.png'});
+        }
+    } catch (err) {
+        console.log(new Date().toString() + ": " + err);
     }
+
+    console.log(new Date().toString() + ": scraping ended");
     return data;
 }
 
-const minutes = 1;
-setInterval( scrapeSeats, 60000 * minutes );
-
 const date = new Date();
-date.getHours;
-date.getMinutes;
-date.getSeconds;
+console.log(date + ": bot started");
+const waitSeconds = (61 - date.getSeconds());
+setTimeout(() => {console.log(new Date().toString() + ": bot finished waiting")}, waitSeconds * 1000);
+
+setInterval(scrapeSeats, 60000);
